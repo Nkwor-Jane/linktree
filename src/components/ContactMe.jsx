74e9a0ac3bb59d5e2edca5b3941c087e845d9fbe
Nkwor-Jane <nkwor.jane.chinelo@gmail.com>
2022-11-05 13:09:24 +0100

    style: Made minor changes to the styling, Used react-hook-form for validation

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -3,63 +3,68 @@ import "../styles/ContactMe.css";
 import I4G from "../images/I4G.png";
 import Zuri from "../images/zuri.png";
 
+import {useForm} from "react-hook-form";
+
 export default function ContactMe () {
+    const {register, handleSubmit} = useForm();
+    const onSubmit = res => console.log(res)
   return (
-    <div className="form_body">
-        <h1>Contact Me</h1>
-        <p>Hi there, contact me to ask me about anything you have in mind.</p>
-        <div>
-            <form>
-                <div className="names">
-                    <div className="minis">
-                        <label htmlFor="first_name" className="labels">First Name</label>
-                        <input required type="text" id="first_name" placeholder="Enter your first name" className="inputs" />
+    <div className="">
+        <div className="form_body">
+            <section>
+                <h1>Contact Me</h1>
+                <p>Hi there, contact me to ask me about anything you have in mind.</p>
+            </section>
+            <section>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="names">
+                        <div className="minis">
+                            <label htmlFor="first_name" className="labels">First Name</label>
+                            <input {...register("first_name")} type="text" id="first_name" placeholder="Enter your first name" className="inputs" />
+                        </div>
+                        
+                        <div className="minis mini_last_name">
+                            <label htmlFor="last_name" className="labels">Last Name</label>
+                            <input {...register("last_name")} type="text" id="last_name" placeholder="Enter your last name"  className="inputs"/>
+                        </div>
                     </div>
+
+                    <div className="minis">
+                        <label htmlFor="email" className="labels">Email</label>
+                        <input {...register("email")} type="email" id="email" placeholder="yourname@email.com"  className="inputs"/>
+                    </div >
                     
                     <div className="minis">
-                        <label htmlFor="last_name" className="labels">Last Name</label>
-                        <input required type="text" id="last_name" placeholder="Enter your last name"  className="inputs"/>
+                        <label htmlFor="meaasge" className="labels">Message</label>
+                        <textarea {...register("message")} name="message"  rows="10" cols="40" id="message" placeholder="Send me a message and I'll reply you as soon as possible..." className="inputs"/>
                     </div>
-                </div>
 
-                <div className="minis">
-                    <label htmlFor="email" className="labels">Email</label>
-                    <input required type="email" id="email" placeholder="yourname@email.com"  className="inputs"/>
-                </div >
-                
+                    <div className="minis checkbox_section">
+                        <input {...register("agreement")} type="radio" name="agreement" id="agreement" />
+                        <label htmlFor="agreement">You agree to providing your data to Jane who may contact you.</label>
+                    </div>
+                    <div>
 
-                <div className="minis">
-                    <label htmlFor="meaasge" className="labels">Message</label>
-                    <textarea required name="message"  id="message" placeholder="Send me a message and I'll reply you as soon as possible..." className="inputs"/>
-                </div>
-
-                <div className="minis checkbox_section">
-                    <input required type="radio" name="agreement" id="agreement" />
-                    <label htmlFor="agreement">You agree to providing your data to
-                    &#123;name&#125;
-                    who may contact you.</label>
-                </div>
-                <div>
-                    
-                </div>
-                <button type="submit" id="btn__submit" className="contact_btn">Send message</button>
-            </form>
+                    </div>
+                    <button type="submit" id="btn__submit" className="contact_btn">Send message</button>
+                </form>
+            </section>
+            
         </div>
-        
         <footer>
-            <hr/>
-            <div className="unique_footer">
-                    <div>
-                        <img src={Zuri} alt="Zuri" />
-                    </div>
-                    <div>
-                        <p>HNG Internship 9 Frontend Task</p>
-                    </div>
-                    <div>
-                        <img src={I4G} alt="I4G" />
-                    </div>
-            </div>
-        </footer>
+                <hr/>
+                <div className="unique_footer">
+                        <div>
+                            <img src={Zuri} alt="Zuri" />
+                        </div>
+                        <div>
+                            <p>HNG Internship 9 Frontend Task</p>
+                        </div>
+                        <div>
+                            <img src={I4G} alt="I4G" />
+                        </div>
+                </div>
+            </footer>
     </div>
   )
 }
